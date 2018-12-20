2a894f625e40a7255ac3091e6ef64dfabd7ac044
Anwesh43 <anweshthecool0@gmail.com>
2018-12-20 11:01:07 +0530

    adding RectLineOpenerStage class

--- rectlineopenerstage/src/main/java/com/anwesh/uiprojects/rectlineopenerstage/RectLinerOpenerStage.kt
+++ rectlineopenerstage/src/main/java/com/anwesh/uiprojects/rectlineopenerstage/RectLinerOpenerStage.kt
@@ -55,3 +55,21 @@ fun Canvas.drawRLONode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class RectLinerOpenerStage(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
