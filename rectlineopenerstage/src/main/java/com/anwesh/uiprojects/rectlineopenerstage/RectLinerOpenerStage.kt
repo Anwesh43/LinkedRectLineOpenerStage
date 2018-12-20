ff0d2896320c030db12335b329b2085f1ed06fc5
Anwesh43 <anweshthecool0@gmail.com>
2018-12-20 10:47:22 +0530

    adding libraries and constants

--- rectlineopenerstage/src/main/java/com/anwesh/uiprojects/rectlineopenerstage/RectLinerOpenerStage.kt
+++ rectlineopenerstage/src/main/java/com/anwesh/uiprojects/rectlineopenerstage/RectLinerOpenerStage.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.rectlineopenerstage
 /**
  * Created by anweshmishra on 20/12/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val color : Int = Color.parseColor("#4527A0")
+val strokeWidth : Int = 90
+val sizeFactor : Float = 2.7f
