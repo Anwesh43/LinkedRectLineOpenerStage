04161ba4c0422cff261980df99918b108b3b2ebf
Anwesh43 <anweshthecool0@gmail.com>
2018-12-20 11:15:44 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectlineopenerstage/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectlineopenerstage/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrectlineopenerstage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rectlineopenerstage.RectLinerOpenerStage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectLinerOpenerStage.create(this)
     }
 }
