a04d9d5016b701f8a4b48d2c11da4c99e27fb982
Anwesh43 <anweshthecool0@gmail.com>
2018-12-20 11:02:42 +0530

    adding State class

--- rectlineopenerstage/src/main/java/com/anwesh/uiprojects/rectlineopenerstage/RectLinerOpenerStage.kt
+++ rectlineopenerstage/src/main/java/com/anwesh/uiprojects/rectlineopenerstage/RectLinerOpenerStage.kt
@@ -67,9 +67,29 @@ class RectLinerOpenerStage(ctx : Context) : View(ctx) {
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale + scale.updateScale(dir, lines, 1)
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
