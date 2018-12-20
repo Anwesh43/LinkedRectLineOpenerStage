5e947526ad7b3ff18c488372d001fd0870b18b8a
Anwesh43 <anweshthecool0@gmail.com>
2018-12-20 10:53:04 +0530

    adding Int and Float extension methods

--- rectlineopenerstage/src/main/java/com/anwesh/uiprojects/rectlineopenerstage/RectLinerOpenerStage.kt
+++ rectlineopenerstage/src/main/java/com/anwesh/uiprojects/rectlineopenerstage/RectLinerOpenerStage.kt
@@ -19,3 +19,13 @@ val scGap : Float = 0.05f
 val color : Int = Color.parseColor("#4527A0")
 val strokeWidth : Int = 90
 val sizeFactor : Float = 2.7f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) = Math.min(n.getInverse(), this - i * n.getInverse()) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor()  * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = scGap * dir * mirrorValue(lines, 1)
