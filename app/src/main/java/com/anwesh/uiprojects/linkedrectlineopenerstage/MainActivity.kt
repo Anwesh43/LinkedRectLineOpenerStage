afab700233dd025003d4bb6f3fe6eb8e1e415ac4
Anwesh43 <anweshthecool0@gmail.com>
2018-12-21 23:53:55 +0530

    adding OnAnimationOnCompleteListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectlineopenerstage/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectlineopenerstage/MainActivity.kt
@@ -3,14 +3,21 @@ package com.anwesh.uiprojects.linkedrectlineopenerstage
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.rectlineopenerstage.RectLinerOpenerStage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RectLinerOpenerStage.create(this)
+        val view : RectLinerOpenerStage = RectLinerOpenerStage.create(this)
         fullScreen()
+        view.addOnAnimationCompleteListener({createToast("completed animation number ${it}")},
+                {createToast("animation number ${it} is reset")})
+    }
+
+    fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
